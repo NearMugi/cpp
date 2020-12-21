f3226c893ddf69bbe9cfb261d371b97dbac72ac1
teppei <niamugi@gmail.com>
2020-12-21 13:05:39 +0000

    add Constructor Sample

---
+++ constructor/main.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+template<typename T>
+class nameObject{
+public:
+  // Default Constructor
+  nameObject(){
+    std::cout << "Default Constructor" << std::endl;    
+  };
+
+  // Copy Constructor
+  nameObject(const nameObject& rhs)
+  : nameValue(rhs.nameValue), objectValue(rhs.objectValue)
+  {
+    std::cout << "Copy Constructor" << std::endl;
+  };
+
+  // Destructor
+  ~nameObject(){
+    std::cout << "Destructor" << std::endl;
+  };
+
+  // Copy Assignment Operator
+  nameObject& operator=(const nameObject& rhs){
+   std::cout << "Copy Assignment Operator" << std::endl; 
+  };
+
+  nameObject(const char* name, const T& value)
+  : nameValue(name), objectValue(value) {
+    std::cout << "Constructor(char*, T)" << std::endl;
+  };
+
+  nameObject(const std::string& name, const T& value)
+  : nameValue(name), objectValue(value) {
+    std::cout << "Constructor(std::string, T)" << std::endl;
+  };
+
+  void printValue(){ 
+    std::cout << nameValue << std::endl;
+    std::cout << objectValue << std::endl;
+  };  
+
+private:
+  std::string nameValue;
+  T objectValue;
+};
+
+
+int main(){
+  nameObject<int> no1("hogehoge", 2);
+  nameObject<int> no2(no1);
+  nameObject<int> no3 = no1;
+  no1.printValue();
+  no2.printValue();
+  no3.printValue();
+};
